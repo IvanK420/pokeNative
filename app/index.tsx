480893244de43fa6e7c6ecea7c87e58d690558f6
ivan <ivan.soupram@gmail.com>
2025-07-26 12:15:13 +0400

    debut

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={styles.container}
     >
       <Text>Ici y a du texte pelo</Text>
-    </View>
+      <Link href="/about"> A propos</Link>
+      <Link href={{pathname: '/pokemon/[id]', params:{id:3}}}> Pokemon 3</Link>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor:"pink",
       }
 })
